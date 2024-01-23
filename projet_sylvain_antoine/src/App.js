cf504928092f2cab6a68aaeb35f2121efb94e7c0
MESTRE Sylvain <mestres.sin@gmail.com>
2024-01-23 12:14:23 +0100

    feat: Getter API

--- projet_sylvain_antoine/src/App.js
+++ projet_sylvain_antoine/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {getData} from "./components/Services/servicesApi";
 
 function App() {
+  const [data, setData] = useState({ Film: { title: '' }, Chapters: [] });
+
+
+  const fetchData = async () => {
+    try {
+      const response = await getData();
+      if (response.data) {
+        const dataApi = response.data;
+        console.log("dataApi title : " + dataApi.Film.title);
+
+        return dataApi
+      } else {
+        // Gérez les erreurs ici (par exemple, réponse non ok)
+        console.error('Erreur lors de la récupération des donnnées 2');
+        return [];
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des donnnées 1 :', error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchData().then(fetchedData => {
+      setData(fetchedData);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Title</h3>
+      <p>{data.Film.title}</p>
+
+      <h3>Chapters</h3>
+        {
+          data.Chapters.map((chapter, index) => (
+            <li key={index}>
+              {chapter.title} (Position: {chapter.pos})
+            </li>
+          ))
+        }
+
+        <h3>Waypoints</h3>
+        {data.Waypoints.map((waypoint, index) => (
+            <li key={index}>
+              {waypoint.label} (Lat: {waypoint.lat}, Lng: {waypoint.lng}, Timestamp: {waypoint.timestamp})
+            </li>
+        ))}
+
+        <h3>Keywords</h3>
+        {data.Keywords.map((keyword, index) => (
+            <div key={index}>
+              <h4>Position: {keyword.pos}</h4>
+
+                {keyword.data.map((item, idx) => (
+                    <li key={idx}>
+                      <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                    </li>
+                ))}
+
+            </div>
+        ))}
+
     </div>
   );
 }
